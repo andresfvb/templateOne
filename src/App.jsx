58fc53354012eacfc7dca6dd4b897a3b5953d2f1
AndresFvb <barandicandres@gmail.com>
2024-01-23 17:14:04 -0500

    Terminamos la cabecera

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Header from './components/Header'
+
+function App() {
+
+  return (
+    <div className="container mx-auto mt-20">
+      <Header></Header>
+    </div>
+  )
+}
+
+export default App
